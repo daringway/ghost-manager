a04dbbc38a40f8e9768e1ccb6d4a4b894c4dcfd9
Walter Derezinski <walter@daringway.com>
2020-12-06 22:48:52 -0500

    added captcha

--- starter/index.js
+++ starter/index.js
@@ -68,7 +68,7 @@ async function displayValidationForm(req, res) {
     <form>
       Your Ghost server is currently stopped.
       <div class="frc-captcha" data-sitekey="${process.env.FRIENDLY_CAPTCHA_SITEKEY}" data-callback="myCallback" data-start="auto"></div>
-      <input type="submit">
+      <input type="submit" hidden=true>
     </form>
        
     </body>
