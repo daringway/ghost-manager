62fc384da94e120114fbb0d7f6b121cc43df0688
Walter Derezinski <walter@daringway.com>
2020-12-06 23:14:10 -0500

    added captcha

--- starter/index.js
+++ starter/index.js
@@ -63,7 +63,7 @@ async function displayValidationForm(req, res) {
       function myCallback(solution) {
         document.getElementById("starter").setAttribute("hidden", false);
         console.log("Captcha finished with solution " + JSON.stringify(solution));
-        console.log("isHidden" document.getElementById("starter".getAttribute("hidden"));
+        console.log("isHidden", document.getElementById("starter".getAttribute("hidden")));
       }
     </script>
     
