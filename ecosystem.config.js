320aeac1cf5e86b703964503a6fcc054467d8481
Walter Derezinski <walter@daringway.com>
2021-07-27 01:33:27 -0400

    bug fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,30 @@
 
 module.exports = {
   apps : [{
-    name: 'backup',
+    name: 'backup-service',
     script: './backup/index.js',
     watch: ['./backup'],
     instances : 1,
     env : {
     }
-  },  {
-    name: 'backup-sched',
-    script: './backup-sched/index.js',
-    watch: ['./backup-sched'],
-    instances : 1,
-    env : {
-      INTERVAL : 3600
-    }
+  },
+  {
+    name: 'backup-cron',
+    script: "bin/site-backup",
+    instances: 1,
+    exec_mode: 'fork',
+    cron_restart: "*/120 * * * *",
+    watch: false,
+    autorestart: false
+  },
+  {
+    name: 'cert-renew',
+    script: "./bin/ghost-cert-renew",
+    instances: 1,
+    exec_mode: 'fork',
+    cron_restart: "7 */24 * * *",
+    watch: false,
+    autorestart: false
   }
   ],
 };
