5f4e5ecd4c68971dffcc487af3669a24f03c3a9a
Walter Derezinski <walter@daringway.com>
2021-08-05 16:03:08 -0400

    new install with ami

--- install.sh
+++ install.sh
@@ -1,46 +1,12 @@
 #!/bin/bash -x
 
-# Install Guidelines from https://ghost.org/docs/install/ubuntu/
-# Leverages https://pm2.keymetrics.io/docs/usage/pm2-doc-single-page/
-
 INSTALL_DIR=/var/www/ghost-serverless
-RELEASE_VERSION=main
-NODE_VERSION=14
-
-if [[ $(id -u) != "0" ]]
-then
-  echo "ERROR: must run as root"
-  exit 2
-fi
-
-START_TS=$(date +%s)
-echo "ghost-serverless ts 0: starting"
-
-# Add Repos
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update
-
-apt-get install -y jq fish unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-
-# change ubuntu to fish, yes really
-sudo chsh -s /usr/bin/fish ubuntu
-
-###### Download ghost serverless ######
-git clone --single-branch https://github.com/daringway/ghost-serverless $INSTALL_DIR
-
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): core packages installed"
 
 while ! aws sts get-caller-identity
 do
   echo "Missing IAM Role or not attached , sleeping 15"
   sleep 15
 done
-
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): AWS creds verified"
 
 # Setup the .env
 $INSTALL_DIR/bin/update-env
@@ -53,14 +19,6 @@ IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 TTL=60 # It currently takes about 2 minutes for the rest of the setup
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$CMS_HOSTNAME'","Type":"A","TTL":'$TTL',"ResourceRecords":[{"Value":"'$IP'"}]}}]}'
 
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): DNS updated"
-
-# Install rest of needed software packages
-curl -sL https://deb.nodesource.com/setup_${NODE_VERSION}.x | bash -
-apt-get install -y yarn  nginx nodejs
-npm install ghost-cli@latest pm2@latest eslint ghost-static-site-generator -g
-
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): all packages installed"
 
 # Setup firewall
 ufw allow 'Nginx Full'
@@ -69,8 +27,6 @@ for DIR in $(ls -d $INSTALL_DIR/services/*)
 do
   (cd $DIR; npm install)
 done
-
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): ghost-serverless npm packages installed"
 
 mkdir -p /var/www/ghost $INSTALL_DIR $WEB_DIR $BACKUP_DIR
 chown -R ubuntu:ubuntu /var/www /home/ubuntu $INSTALL_DIR
@@ -81,8 +37,6 @@ chmod 775 /var/www/ghost
 # curl https://get.acme.sh | sh -s email=certs@daringway.com --home /etc/letsencrypt/
 # Wrap this in a webserver so you can see the status from the browser (Or at least give steps and not full logs)
 su ubuntu -c $INSTALL_DIR/bin/site-restore
-
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): ghost site restored"
 
 # Setup temp nginx to status service
 # 1st restore site (not version)
